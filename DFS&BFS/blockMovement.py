d3900f8e022d6ade5f8c6b93ef26d21ef4f8037c
Ohjigyun <epraise12@naver.com>
2022-03-02 22:55:47 +0900

    3/2 Algorithm : BFS

--- DFS&BFS/blockMovement.py
+++ DFS&BFS/blockMovement.py
@@ -46,8 +46,163 @@ def solution(board):
                 visited.append(next_pos)
     return 0
     
-                            
-    
+# 프로그래머스 자바스크립트 답안(양대환님)
+# // 따라함 : https://jennylee4517.github.io/ps/2020%EC%B9%B4%EC%B9%B4%EC%98%A4%EA%B3%B5%EC%B1%84-5/ 
+
+# function solution(map) {
+#     const n = map.length,
+#       dx = [0, 0, 1, -1],
+#       dy = [1, -1, 0, 0],
+#       rowToColDx = [-1, -1, 0, 0],
+#       rowToColDy = [0, 1, 0, 1],
+#       colToRowDx = [0, 1, 0, 1],
+#       colToRowDy = [0, 0, -1, -1],
+#       q = [
+#         [0, 0, 0, 0]
+#       ],
+#       visited = [];
+
+#     for (let i = 0; i < n; i++) {
+#       visited.push([]);
+#       for (let j = 0; j < n; j++) {
+#         visited[i].push([Infinity, Infinity]);
+#       }
+#     }
+
+#     visited[0][0][0] = 0;
+
+#     let min = Infinity;
+
+#     const canGoRight = (x, y, p) => {
+#       if(p === 0) {
+#         if(y+2 >= n || map[x][y+1] === 1 || map[x][y+2] === 1)
+#           return false;
+#       }else {
+#         if(y+1 >= n || x+1 >= n || map[x][y+1] === 1 || map[x+1][y+1] === 1)
+#           return false;
+#       }
+#       return true;
+#     };
+
+#     const canGoLeft = (x, y, p) => {
+#       if(p === 0) {
+#         if(y-1 < 0 || map[x][y-1] === 1)
+#           return false;
+#       }else {
+#         if(y-1 < 0 || x+1 >= n || map[x][y-1] === 1 || map[x+1][y-1] === 1)
+#           return false;
+#       }
+#       return true;
+#     };
+
+#     const canGoDown = (x, y, p) => {
+#       if(p === 0) {
+#         if(x+1 >= n || y+1 >= n || map[x+1][y] === 1 || map[x+1][y+1] === 1)
+#           return false;
+#       }else {
+#         if(x+2 >= n || map[x+1][y] === 1 || map[x+2][y] === 1) {
+#           return false;
+#         }
+#       }
+#       return true;
+#     };
+
+#     const canGoUp = (x, y, p) => {
+#       if(p === 0) {
+#         if(x-1 < 0 || y+1 >= n || map[x-1][y] === 1 || map[x-1][y+1] === 1)
+#           return false;
+#       }else {
+#         if(x-1 < 0 || map[x-1][y] === 1) {
+#           return false;
+#         }
+#       }
+#       return true;
+#     };
+
+#     const canRotateUp = (x, y) =>
+#       !(x - 1 < 0 || y + 1 >= n || map[x - 1][y] === 1 || map[x - 1][y + 1] === 1);
+
+#     const canRotateDown = (x, y) =>
+#       !(x + 1 >= n || y + 1 >= n || map[x + 1][y] === 1 || map[x + 1][y + 1] === 1);
+
+#     const canRotateRight = (x, y) =>
+#       !(y + 1 >= n || x + 1 >= n || map[x][y + 1] === 1 || map[x + 1][y + 1] === 1);
+
+#     const canRotateLeft = (x, y) =>
+#       !(y - 1 < 0 || x + 1 >= n || map[x][y - 1] === 1 || map[x + 1][y - 1] === 1);
+
+#     const canGoMap = [
+#       canGoRight,
+#       canGoLeft,
+#       canGoDown,
+#       canGoUp
+#     ],
+#       canRotateMap = [
+#         [
+#           canRotateUp,
+#           canRotateUp,
+#           canRotateDown,
+#           canRotateDown
+#         ], [
+#           canRotateRight,
+#           canRotateRight,
+#           canRotateLeft,
+#           canRotateLeft
+#         ],
+#       ],
+
+#       deltaMap = [
+#         [rowToColDx, rowToColDy],
+#         [colToRowDx, colToRowDy]
+#       ];
+
+#     while (q.length) {
+#       const [x, y, t, p] = q.shift();
+
+#       // 만일 (n,n)에 도착했다면 최소시간을 갱신합니다
+#       if (
+#         (x === n - 1 && y === n - 2 && p === 0)
+#         || (x === n - 2 && y === n - 1 && p === 1)
+#       ) {
+#         min = Math.min(min, t);
+#       }
+
+#       // 회전 없이 우좌하상으로 움직일때
+#       for (let k = 0; k < 4; k++) {
+#         if (!canGoMap[k](x, y, p)) {
+#           continue;
+#         }
+
+#         const nx = x + dx[k],
+#           ny = y + dy[k];
+
+#         if (visited[nx][ny][p] > t + 1) {
+#           visited[nx][ny][p] = t + 1;
+#           q.push([nx, ny, t + 1, p]);
+#         }
+#       }
+
+#       for (let k = 0; k < 4; k++) {
+#         // 가로로 놓인 상황인 경우 세로로 돌려보자
+#         // 세로로 놓인 상황인 경우 가로로 돌려보자
+
+#         if (!canRotateMap[p][k](x, y)) continue;
+
+#         const nx = x + deltaMap[p][0][k],
+#           ny = y + deltaMap[p][1][k],
+#           _p = p ? 0 : 1;
+
+#         if (visited[nx][ny][_p] > t + 1) {
+#           visited[nx][ny][_p] = t + 1;
+#           q.push([nx, ny, t + 1, _p]);
+#         }
+#       }
+
+#     }
+
+#     return min;
+#   }
+
     
     
     
